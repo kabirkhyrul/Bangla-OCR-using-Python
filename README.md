41d1c8ba7c9168f49c945ed3c14ed540affc3292
Khyrul Kabir <khyrulkabir01@gmail.com>
2020-09-17 23:48:08 +0600

    Excel Output

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 
 > Python-tesseract is a wrapper for Google’s Tesseract-OCR Engine. It is also useful as a stand-alone invocation script to tesseract, as it can read all image types supported by the Pillow and Leptonica imaging libraries, including jpeg, png, gif, bmp, tiff, and others. Additionally, if used as a script, Python-tesseract will print the recognized text instead of writing it to a file.
 
-By this repo, you will get printout of the recognized text in console and also an output to browser
+By this repo, you will get printout of the recognized text in console and also an output 
+ - to browser
+ - to Excel file
+ - to txt file
 
 ## Installation
 
@@ -15,9 +18,15 @@ pip install opencv-python
 
 
 pip install pytesseract
+
+
+pip install XlsxWriter     (For excel)
 ```
 
 Install [Tesseract OCR ](https://github.com/tesseract-ocr/tesseract)
+
+
+
 
 ## Release your python
 
--- run.py
+++ run.py
@@ -9,6 +9,9 @@ import webbrowser
 
 import os
 
+#  For Excel (optional). Comment out this line
+import xlsxwriter
+from datetime import date
 
 # read imgae
 image = cv2.imread('Untitled.png')
@@ -19,16 +22,37 @@ gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 # get text
 data = pytesseract.image_to_string(gray, lang='ben')
 
-#write data to file with unicode
-with open("output.html", "w", encoding="utf-8") as f:
+# ========================= Excel Output ====================================
+# Create an new Excel file and add a worksheet.
+# workbook = xlsxwriter.Workbook(date.today().isoformat()+'.xlsx');
+# worksheet = workbook.add_worksheet()
+
+# Widen the first column to make the text clearer.
+# worksheet.set_column('A:A', 20)
+
+#Making Array from data String
+# arr = data.split('\n')
+
+# for k, v in enumerate(arr):   
+# 	worksheet.write('A'+ str(k+1), v)
+
+# workbook.close()
+
+# ======================== File Output ======================================
+# write data to file with unicode
+with open(date.today().isoformat()+".txt", "w", encoding="utf-8") as f:
 	f.write("<pre>"+ data + "</pre>")
 	f.close()	
 
 # print output in console
 print(data)
 
+#HTML Output
+
 # get file path
-filename = 'file:///'+os.getcwd()+'/' + 'output.html'
+# filename = 'file:///'+os.getcwd()+'/' + date.today().isoformat()+'.html'
 
 # open in new tab
-webbrowser.open(filename, new=2)  
+# webbrowser.open(filename, new=2)  
+
+
